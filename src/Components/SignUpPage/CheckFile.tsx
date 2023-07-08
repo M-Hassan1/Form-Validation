31b45fd463b878fbe842df1e92fdbc5016a8e82a
M-Hassan1 <hassan81932@gmail.com>
2023-07-08 22:23:21 +0500

    chnaged my file

---
+++ src/Components/SignUpPage/CheckFile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckFile = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default CheckFile
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Login from '@/Components/LoginPage/Login'
+import CheckFile from '@/Components/SignUpPage/CheckFile'
 import SignUp from '@/Components/SignUpPage/SignUp'
+
 
 export default function Home() {
   return (
     <div>
+      <CheckFile />
     <SignUp />
     <Login />
     </div>
