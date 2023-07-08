a3500fa56eee9152c12c4d897f0494a8bd644626
M-Hassan1 <hassan81932@gmail.com>
2023-07-09 03:10:03 +0500

    testing

--- src/Components/LoginPage/Login.tsx
+++ src/Components/LoginPage/Login.tsx
@@ -94,27 +94,13 @@
 
 
 
-
-// 868953276567-laft8tetbipop7e3u7emv0ebvml56vvq.apps.googleusercontent.com
-
-// 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
-
-
-
-// AIzaSyCR9E11sKC37B5Q2ZeyhneAegsGVOMeu1M
-
-
-
-
-
-
-
 import { BiSolidHide, BiShow } from 'react-icons/bi';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useFormik } from 'formik';
 import { formSchema } from "./Validation";
 import { useState } from 'react';
 import jwtDecode from 'jwt-decode';
+import SignUp from '../SignUpPage/SignUp';
 
 interface InitialValues {
   userName: string;
@@ -128,7 +114,8 @@ const initialValues: InitialValues = {
 
 const Login = () => {
   const [submitted, setSubmitted] = useState(false);
-  const { values, errors, handleBlur, handleChange, touched, handleSubmit, setValues } = useFormik({
+  const [loggedIn, setLoggedIn] = useState(false); // New state for tracking login status
+  const { values, errors, handleBlur, handleChange, touched, handleSubmit } = useFormik({
     initialValues: initialValues,
     validationSchema: formSchema,
     onSubmit: (values, action) => {
@@ -138,43 +125,77 @@ const Login = () => {
     }
   });
 
-  const clientId = "868953276567-nrshalq5pd2bqdjng3r64c9htr5dp7u1.apps.googleusercontent.com"; // Replace "YOUR_CLIENT_ID" with your actual client ID
+  const clientId = "user client id"; // Replace with your actual client ID
 
   const handleGoogleLoginSuccess = (credentialResponse: any) => {
     console.log("Google login success:", credentialResponse);
-    const { profileObj } = credentialResponse;
-    const { email, name } = profileObj;
-    setValues({ ...values, userName: email, userPassword: name });
+    const details = jwtDecode(credentialResponse.credential);
+    console.log(details);
+    setLoggedIn(true); // Set login status to true after successful login
   };
 
   const handleGoogleLoginError = () => {
     console.log("Google login failed");
   };
 
+  if (loggedIn) {
+    return (
+      <div>
+        <SignUp />
+
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-center h-screen w-full items-center">
       {/* Message After Submit Form */}
-      {submitted && <p className='absolute top-0 p-4 text-green-800 bg-gray-300 ' >Form submitted successfully!</p>}
+      {submitted && (
+        <p className="absolute top-0 p-4 text-green-800 bg-gray-300">
+          Form submitted successfully!
+        </p>
+      )}
 
-      <form onSubmit={handleSubmit} className="w-[28em] bg-slate-200 h-[33em] relative rounded-md shadow-md flex flex-col items-center justify-around ">
-        <h1 className="text-3xl font-sans text-pink-400 ">
-          Login Page
-        </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="w-[28em] bg-slate-200 h-[33em] relative rounded-md shadow-md flex flex-col items-center justify-around "
+      >
+        <h1 className="text-3xl font-sans text-pink-400">Login Page</h1>
 
         <input
-          className="w-[72%] h-[2em] border-b outline-none border-black text-xl bg-transparent" type="text" placeholder="UserName" name="userName" value={values.userName} onChange={handleChange} autoComplete='off' onBlur={handleBlur}
+          className="w-[72%] h-[2em] border-b outline-none border-black text-xl bg-transparent"
+          type="text"
+          placeholder="UserName"
+          name="userName"
+          value={values.userName}
+          onChange={handleChange}
+          autoComplete="off"
+          onBlur={handleBlur}
         />
-        {errors.userName && touched.userName ? (<p className='text-red-500 -mt-10'>{errors.userName}</p>) : null}
+        {errors.userName && touched.userName ? (
+          <p className="text-red-500 -mt-10">{errors.userName}</p>
+        ) : null}
         <input
-          className="w-[72%] h-[2em] border-b outline-none border-black text-xl bg-transparent" type="password" placeholder="Password" name="userPassword" autoComplete='off' value={values.userPassword} onChange={handleChange} onBlur={handleBlur}
+          className="w-[72%] h-[2em] border-b outline-none border-black text-xl bg-transparent"
+          type="password"
+          placeholder="Password"
+          name="userPassword"
+          autoComplete="off"
+          value={values.userPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
 
-        {errors.userPassword && touched.userPassword ? (<p className='text-red-500 -mt-10'>{errors.userPassword}</p>) : null}
+        {errors.userPassword && touched.userPassword ? (
+          <p className="text-red-500 -mt-10">{errors.userPassword}</p>
+        ) : null}
         <div className="absolute right-20 top-52 cursor-pointer">
           <BiSolidHide size={21} />
         </div>
-        <button type='submit'
-          className="w-[30%] text-xl text-white h-[42px] rounded-md bg-blue-500 hover:bg-blue-600 active:bg-blue-800 duration-300">
+        <button
+          type="submit"
+          className="w-[30%] text-xl text-white h-[42px] rounded-md bg-blue-500 hover:bg-blue-600 active:bg-blue-800 duration-300"
+        >
           Login
         </button>
         <p className="mt-4">Login With</p>
@@ -196,4 +217,15 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+
+
+
+
+
+
+
+
 
--- src/Components/SignUpPage/SignUp.tsx
+++ src/Components/SignUpPage/SignUp.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div></div>
+    <div><h1>Welcome to the new page!</h1>
+    <p>This is the content after successful login.</p></div>
   )
 }
 
