4a7d513b8bd9303bd3c8eaadad8228dda3e49bab
M-Hassan1 <hassan81932@gmail.com>
2023-07-19 08:11:16 +0500

    changed fire base

---
+++ firebase/firebaseConfig.tsx
@@ -0,0 +1,45 @@
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB5gdpuC9mTiDaoJ7omgwHlrMG3EUz4Ugo",
+//   authDomain: "calm-safeguard-392113.firebaseapp.com",
+//   projectId: "calm-safeguard-392113",
+//   storageBucket: "calm-safeguard-392113.appspot.com",
+//   messagingSenderId: "868953276567",
+//   appId: "1:868953276567:web:450ae73b1d087f0ba029d2",
+//   measurementId: "G-BL5BCMZ9C5"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app)
+// export default app
+
+
+
+
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpQCyxTXX2fQ_t69f7WEu5ku7JIWZ7OAE",
+  authDomain: "multi-media-36807.firebaseapp.com",
+  projectId: "multi-media-36807",
+  storageBucket: "multi-media-36807.appspot.com",
+  messagingSenderId: "52986937612",
+  appId: "1:52986937612:web:0392a2a8ec415dc05bc929",
+  measurementId: "G-XS8L3L3R6F"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export default app
